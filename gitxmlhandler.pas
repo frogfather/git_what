590d28536f5d148dfc3cf6b0f93a84ed447e622b
frogfather <j.a.campbell@icloud.com>
2022-10-29 11:49:08 +0100

    git xnl handler clas

---
+++ gitxmlhandler.pas
@@ -0,0 +1,18 @@
+unit gitXMLHandler;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,xml_doc_handler;
+type
+  TGitXMLHandler = class(TXMLDocumentHandler)
+    private
+    public
+  end;
+
+implementation
+
+end.
+
